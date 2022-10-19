e097e767c3ae817e79f2bb3b6acfea854c26b5d0
Over <franciscosebastianyjunior@hotmail.com>
2022-10-19 02:42:16 -0300

    scan_vertical template.

--- src/main.c
+++ src/main.c
@@ -4,12 +4,31 @@
 #define MATRIX_SIZE 3
 #define MAX_WORDS 1
 
+typedef enum {
+    UP,
+    DOWN,
+} VerticalDirection;
+
+typedef enum {
+    LEFT,
+    RIGHT,
+} HorizontalDirection;
+
+typedef struct {
+    int row;
+    int col;
+} Position;
+
+void scan_vertical(char matrix[MATRIX_SIZE][MATRIX_SIZE], char scan_word[MATRIX_SIZE], VerticalDirection dir) {
+	
+}
+
 int main() {
     char matrix[MATRIX_SIZE][MATRIX_SIZE] = {
-		{'F', 'O', 'O'},
-		{'O', 'H', 'A'},
-		{'O', 'J', 'S'},
-	};
+        {'F', 'O', 'O'},
+        {'O', 'H', 'A'},
+        {'O', 'J', 'S'},
+    };
 
     char words[MAX_WORDS][MATRIX_SIZE] = {"FOO"};
 
