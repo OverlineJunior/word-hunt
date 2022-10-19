4d112b0e44caa69acb9d42a778d0b477b5436376
Over <franciscosebastianyjunior@hotmail.com>
2022-10-19 03:13:27 -0300

    prototype scan_vertical.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<stdbool.h>
 #include <string.h>
 
 #define MATRIX_SIZE 3
@@ -24,8 +25,22 @@ typedef struct {
     Position first_pos;
 } ScanResult;
 
-ScanResult scan_vertical(char matrix[MATRIX_SIZE][MATRIX_SIZE], char scan_word[MATRIX_SIZE], VerticalDirection dir) {
-	
+// TODO: What if there are 2 occurrences of scan_word in a single vertical line?
+// TODO: Make use of dir.
+ScanResult scan_vertical(char matrix[MATRIX_SIZE][MATRIX_SIZE], char scan_word[MATRIX_SIZE], int col, VerticalDirection dir) {
+    int count = 0;
+
+    for (int i = 0; scan_word[i] != '\0'; i++) {
+        char ch = matrix[i][col];
+        char scan_ch = scan_word[i];
+
+        if (ch != scan_ch) break;
+
+        count++;
+    }
+
+    ScanResult res;
+    return res;
 }
 
 int main() {
@@ -35,7 +50,10 @@ int main() {
         {'O', 'J', 'S'},
     };
 
-    char words[MAX_WORDS][MATRIX_SIZE] = {"FOO"};
+    char words[MAX_WORDS][MATRIX_SIZE + 1] = {"FOO"};
+
+    VerticalDirection dir = DOWN;
+    scan_vertical(matrix, words[0], 0, dir);
 
     return 0;
 }
