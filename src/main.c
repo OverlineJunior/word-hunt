5bf40742952f860d8aede6ccd75354f9c2abecda
Over <franciscosebastianyjunior@hotmail.com>
2022-10-19 02:38:28 -0300

    define matrix and words.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MATRIX_SIZE 3
+#define MAX_WORDS 1
 
 int main() {
-    printf("Hello, world!");
+    char matrix[MATRIX_SIZE][MATRIX_SIZE] = {
+		{'F', 'O', 'O'},
+		{'O', 'H', 'A'},
+		{'O', 'J', 'S'},
+	};
+
+    char words[MAX_WORDS][MATRIX_SIZE] = {"FOO"};
 
     return 0;
 }
